74bb65ef59a05bd5c383a400b1b10004bf066468
Zergui <stephane.forlini@gmail.com>
2018-12-01 18:28:04 +0100

    helpers

---
+++ 2018/readfromfile.h
@@ -0,0 +1,31 @@
+#ifndef H_INCLUDED_READFROMFILE
+#define H_INCLUDED_READFROMFILE
+
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    inline char* readfromfile(char* filename) {
+
+    FILE* file = fopen(filename, "r");
+    char* content;
+    size_t n = 0;
+    int c;
+
+    if (file == NULL)
+        return NULL;
+
+    fseek(file, 0, SEEK_END);
+    long filesize = ftell(file);
+
+    fseek(file, 0, SEEK_SET);
+    content = (char*) malloc(filesize);
+
+    while ((c = fgetc(file)) != EOF)
+        content[n++] = (char) c;
+
+    content[n] = '\0';
+
+    return content;
+}
+
+#endif
