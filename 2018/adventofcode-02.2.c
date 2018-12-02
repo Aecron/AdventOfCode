d81db76e5b972ff30707ef3a720f62803794390d
Zergui <stephane.forlini@gmail.com>
2018-12-02 18:24:07 +0100

    day2.2

--- 2018/adventofcode-02.2.c
+++ 2018/adventofcode-02.2.c
@@ -29,14 +29,10 @@ int main() {
 
         sscanf(input + i, "%s\n", baseString);
 
-        for (j = i + 28 ; j < strlen(input) ; j += 28) {
-
-            if (sscanf(input + j, "%s", compareString) == 1) {
-
+        for (j = i + 28 ; j < strlen(input) ; j += 28)
+            if (sscanf(input + j, "%s", compareString) == 1)
                 if (diffChars(baseString, compareString) == 1)
                     printf("Found the 2 strings:\n\t%s\n\t%s\n", baseString, compareString);
-            }
-        }
     }
 
     free(input);
